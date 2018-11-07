c2f5c91c94b577868e4cf2a800bb224705c2b56e
mnswaleh <mnswaleh@gmail.com>
2018-11-07 15:49:11 +0300

    [finish #161698905] get orders by specific user

---
+++ app/api/v1/views/users_view.py
@@ -0,0 +1,17 @@
+from flask import Flask, make_response, jsonify, request
+from flask_restful import Resource, reqparse
+from app.api.v1.models.users_model import UsersModel
+from app.api.v1.models.orders_model import OrdersModel
+
+
+class UserOrders(Resource, UsersModel, OrdersModel):
+
+    def __init__(self):
+        self.db = UsersModel()
+        self.orders = OrdersModel()
+
+    def get(self, userId):
+        user = self.db.get_user(userId)
+        result = self.orders.get_user_orders(userId)
+
+        return make_response(jsonify({"Title": "Delivery orders by " + user['username'], "Delivery orders list": result}))
