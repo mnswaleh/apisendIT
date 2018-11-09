fd2acbed16e246852a0bb4ea8707a7bcf9ab72dc
mnswaleh <mnswaleh@gmail.com>
2018-11-10 01:25:27 +0300

    [starts #161851928] create user endpoint

--- app/api/v1/models/orders_model.py
+++ app/api/v1/models/orders_model.py
@@ -1,32 +1,25 @@
+"""Orders Models"""
+
 from datetime import date
 from .users_model import UsersModel
-orders = [{
-    "order id": 1,
-    "order no": "678356",
-    "date created": "23/07/2018",
-    "pick up location": "nanyuki",
-    "delivery location": "nairobi",
-    "current location": "kikuyu",
-    "weight": "2kg",
-    "price": "2000",
-    "status": "in transit",
-    "sender": 1
-}]
+orders = []
 
 
-class OrdersModel(object):
+class OrdersModel():
+    """Create Orders Model"""
 
     def __init__(self):
-        self.user = UsersModel()
-        self.users = self.user.get_users()
+        self.users_db = UsersModel()
+        self.order_db = orders
 
     def create_order(self, order_no, pick_up, delivery, weight, price, sender):
+        """Create order and append it to orders"""
         today = date.today()
 
         created = today.strftime("%d/%m/%Y")
 
         order = {
-            "order id": len(orders) + 1,
+            "order id": len(self.order_db) + 1,
             "order no": order_no,
             "date created": created,
             "pick up location": pick_up,
@@ -38,23 +31,27 @@ class OrdersModel(object):
             "sender": sender
         }
 
-        orders.append(order)
+        self.order_db.append(order)
 
         return order
 
     def get_orders(self):
-        for order in orders:
-            for user in self.users:
+        """Get orders in database"""
+        users = self.users_db.get_users()
+
+        for order in self.order_db:
+            for user in users:
                 if user['user id'] == order['sender']:
                     order['sender'] = user['username']
                     break
 
-        return orders
+        return self.order_db
 
     def get_order(self, order_id):
+        """Get a specific order from database"""
         result = {"message": "order unknown"}
 
-        for order in orders:
+        for order in self.order_db:
             if order['order no'] == order_id:
                 result = order
                 break
@@ -62,9 +59,10 @@ class OrdersModel(object):
         return result
 
     def update_order(self, order_id, location, status):
+        """update order details"""
         result = {"message": "order unknown"}
 
-        for order in orders:
+        for order in self.order_db:
             if order['order no'] == order_id:
                 order['current location'] = location
                 order['status'] = status
@@ -74,9 +72,10 @@ class OrdersModel(object):
         return result
 
     def cancel_order(self, order_id):
+        """Cancel delivery order"""
         result = {"message": "order unknown"}
 
-        for order in orders:
+        for order in self.order_db:
             if order['order no'] == order_id:
                 order['status'] = "canceled"
                 result = order
@@ -85,9 +84,10 @@ class OrdersModel(object):
         return result
 
     def change_delivery(self, order_id, delivery_location):
+        """change delivery location"""
         result = {"message": "order unknown"}
 
-        for order in orders:
+        for order in self.order_db:
             if order['order no'] == order_id:
                 order['current location'] = delivery_location
                 result = order
@@ -96,10 +96,13 @@ class OrdersModel(object):
         return result
 
     def get_user_orders(self, user_id):
+        """Get orders created by specific order"""
+        users = self.users_db.get_users()
+
         user_orders = []
-        for order in orders:
+        for order in self.order_db:
             if order['sender'] == user_id:
-                for user in self.users:
+                for user in users:
                     if user['user id'] == order['sender']:
                         order['sender'] = user['username']
                         break
@@ -108,11 +111,15 @@ class OrdersModel(object):
         return user_orders
 
     def get_delivered_orders(self, user_id):
-        user_orders = [order for order in orders if (order['sender'] == user_id and order['status'] == 'delivered')]
+        """Get delivered orders for a specific user"""
+        user_orders = [order for order in self.order_db if (
+            order['sender'] == user_id and order['status'] == 'delivered')]
 
         return len(user_orders)
 
     def get_orders_in_transit(self, user_id):
-        user_orders = [order for order in orders if (order['sender'] == user_id and order['status'] == 'in transit')]
+        """Get orders in transit by a specific user"""
+        user_orders = [order for order in self.order_db if (
+            order['sender'] == user_id and order['status'] == 'in transit')]
 
         return len(user_orders)
